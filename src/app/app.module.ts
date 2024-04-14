b411c2a8e18f4b959c11a6b38eeda87505a974a9
Shoshone Arnold <42348679+shoshonearnold@users.noreply.github.com>
2024-04-13 18:52:11 -0600

    added home page (#2)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeadquatersComponent } from './headquaters/headquaters.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadquatersComponent
   ],
   imports: [
     BrowserModule,
